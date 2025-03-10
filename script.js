34db27c66eb622eee6d4b3599314b63e1616f320
123456kwanele <135286987+123456kwanele@users.noreply.github.com>
2025-03-10 15:27:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.getElementById('country-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+  const countryName = document.getElementById('country-name').value.trim();
+  const countryInfoSection = document.getElementById('country-info');
+  const borderingCountriesSection = document.getElementById('bordering-countries');
+  if (!countryName) {
+        alert('Please enter a country name.');
+        return;
+    }
 
+  
+
+  
+});
